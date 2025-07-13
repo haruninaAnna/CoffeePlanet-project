75f335e971d8cc53aab4ec55c47cc46777ed4abe
haruninaAnna <haruninaanna28@gmail.com>
2025-07-13 18:34:54 +0200

    created blog page

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-console.log('Hello, world!');
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const openBtn = document.getElementById('openBookTableModal');
+  const modal = document.getElementById('modal-book-table');
+  const closeBtn = modal.querySelector('.close-button');
+
+  openBtn.addEventListener('click', () => {
+    modal.style.display = 'flex'; // или 'block' — зависит от твоей CSS-разметки
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
